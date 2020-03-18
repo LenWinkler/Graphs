635b57500016645e56bdf714f0d9ec629f39b346
LenWinkler <lenwinkler86@gmail.com>
2020-03-18 02:23:13 -0400

    finished earliest_ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,76 +1,57 @@
 from graph import Graph
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
 
-    # graph
+    # build graph
+
     g = Graph()
-    # set to store visited
-    # visited = set()
+
     # add vertices
     for ancestor in ancestors:
-        # if ancestor[0] not in visited:
         g.add_vertex(ancestor[0])
-        # visited.add(ancestor[0])
-        # if ancestor[1] not in visited:
         g.add_vertex(ancestor[1])
-        # visited.add(ancestor[1])
+
     # add edges
     for ancestor in ancestors:
-        print('vertices before', g.vertices)
         g.add_edge(ancestor[1], ancestor[0])
-        print('vertices after', g.vertices)
 
-    
+    # # traverse graph (BFT)
 
-    # # TRAVERSE GRAPH (DFT)
+    # Create a queue
+    q = Queue()
+    # create a list to hold paths
+    paths_list = []
+    # Enqueue path the starting node
+    q.enqueue([starting_node])
+    # While the queue is not empty...
+    while q.size() > 0:
+        # Dequeue first path
+        path = q.dequeue()
+        paths_list.append(path)
+        # grab last vertex from path
+        last_vert = path[-1]
+        # Enqueue a path to the parents of its last element
+        for parent in g.get_neighbors(last_vert):
+            # make a copy of the path and add parent to end of path
+            path_copy = path.copy()
+            path_copy.append(parent)
+            # ENQUEUE THE COPY
+            q.enqueue(path_copy)
 
-    # keep track of furthest ancestor(s)
-    
+    # vars for loop
+    longest_path = -1
+    longest_list = -1
+    earliest_ancestor = -1
 
-    # create stack
-    
+    # if list length is <= 1 (starting node has no parents), return earliest_ancestor as -1
+    if len(paths_list) <=1:
+        return earliest_ancestor
 
-    # push starting_node
-    
+    # find longest list(s) and earliest ancestor in paths_list
+    for i in range(len(paths_list)):
+        if len(paths_list[i]) > longest_path:
+            longest_path = len(paths_list[i])
+            earliest_ancestor = paths_list[longest_list][-1]
 
-    # while stack is not empty
-    
-
-
-
-    # # BUILD GRAPH
-
-    # # dictionary to store vertices
-    # vertices = {}
-    # # add vertices
-    # for ancestor in ancestors:
-    #     if ancestor[0] not in vertices:
-    #         vertices[ancestor[0]] = set()
-
-    #     if ancestor[1] not in vertices:
-    #         vertices[ancestor[1]] = set()
-    #     else:
-    #         vertices[ancestor[1]].add(ancestor[0])
-    # # add edges
-    # for ancestor in ancestors:
-    #     vertices[ancestor[1]].add(ancestor[0])
-    # print('graph', vertices)
-    # print('return empty', vertices[9])
-
-
-    # # # TRAVERSE GRAPH (DFT)
-
-    # # keep track of furthest ancestor(s)
-    # furthest_ancestor = -1
-
-    # # create stack
-    # s = Stack()
-
-    # # push starting_node
-    # s.push(starting_node)
-
-    # # while stack is not empty
-    # while s.size() > 0:
-    #     # pop first node
-    #     n = s.pop()
-    #     # 
+    return earliest_ancestor
