08b01ab655ba69958df5d19b1fedd2207fcf4009
LenWinkler <lenwinkler86@gmail.com>
2020-04-14 17:30:48 -0400

    finished ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,5 @@
 from graph import Graph
+from util import Queue
 
 def earliest_ancestor(ancestors, starting_node):
     
@@ -16,14 +17,46 @@ def earliest_ancestor(ancestors, starting_node):
     for pair in ancestors:
         graph.add_edge(pair[1], pair[0])
 
-    print(graph.vertices)
+    # return -1 if starting_node has no parents
+    if not graph.get_neighbors(starting_node):
+        return -1
     
-    # do a BFT on the graph starting at starting_node
-    # if starting_node has no parents(neighbors), return -1
+    # we'll do a BFT on the graph, starting at starting_node
+
+    # create queue and enqueue starting_node
+    queue = Queue()
+    queue.enqueue([starting_node])
     # store a list of paths(lists)
+    paths = []
+
+    while queue.size() > 0:
+        # dequeue path
+        path = queue.dequeue()
+        # add it to paths list
+        paths.append(path)
+        # enqueue parents
+        for parent in graph.get_neighbors(path[-1]):
+            # copy path and add parent
+            new_path = path + [parent]
+            # enqueue that new path
+            queue.enqueue(new_path)
+
     # at the end, return last element of longest path
     # if multiple paths are tied for longest, return the lesser ancestor(numerically)
-    
-test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    # use for loop to find longest path(s)
 
-earliest_ancestor(test_ancestors, 1)
+    longest_length = 0
+    earliest_ancestor = -1
+
+    for path in paths:
+        # if path length is greater, we know we have a new earliest ancestor
+        if len(path) > longest_length:
+            longest_length = len(path)
+            earliest_ancestor = path[-1]
+        # if equal, we need to pick the ancestor with the lowest numerical value
+        if len(path) == longest_length:
+            if path[-1] < earliest_ancestor:
+                earliest_ancestor = path[-1]
+
+
+    return earliest_ancestor
